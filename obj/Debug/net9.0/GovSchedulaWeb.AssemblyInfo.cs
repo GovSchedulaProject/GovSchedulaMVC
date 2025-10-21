435254562eacb598d76e0585eb93f415cd82d77a
Justin Mensah <jstnmensa111@gmail.com>
2025-10-21 01:01:19 +0000

    Remove bin and obj folders from tracking

--- obj/Debug/net9.0/GovSchedulaWeb.AssemblyInfo.cs
+++ obj/Debug/net9.0/GovSchedulaWeb.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GovSchedulaWeb")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3c82093d139026ba3f5ec9434c5cf02976a747a4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2726c96381acfc3cffab9c8edeaee7aa4536eb58")]
 [assembly: System.Reflection.AssemblyProductAttribute("GovSchedulaWeb")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GovSchedulaWeb")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
